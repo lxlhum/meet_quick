718bb05d3c8642757a439aa3c93a28831f0f2515
yangttlxl@sina.com <yangttlxl@sina.com>
2017-03-31 17:01:29 +0800

    提交

--- handlers/login.js
+++ handlers/login.js
@@ -12,28 +12,34 @@ exports.loginAction = function (req, res, next) {
 }
 
 exports.tempAddUser = function (req, res, next) {
-    
+
     var username = "meet_ac20110317_admin"
     var password = "Cx_20110317_acmeet";
-    var shaSum = crypto.createHash('sha256');
-    shaSum.update(password);
 
-    
+    var shaSum_username = crypto.createHash('md5');
+    shaSum_username.update(username);
+    var hashedUsername = shaSum_username.digest('hex');
+    console.log(hashedUsername);
+    var shaSum_password = crypto.createHash('md5');
+    shaSum_password.update(password);
+    var hashedPassword = shaSum_password.digest('hex');
+    console.log(hashedPassword);
 
-    // var user = new User({
-    //     username: 'meet_cdfff',
-    //     password: 'fghjj'
-    // });
-    // user.save(function (err, response) {
+    var user = new User({
+        username: hashedUsername,
+        password: hashedPassword,
+        lastlogintime: new Date()
+    });
+    user.save(function (err, response) {
 
-    //     if (err) {
-    //         console.log("保存失败" + err);
-    //     }
-    //     else {
-    //         console.log("Res:" + response);
-    //         res.send('保存成功');
-    //     }
+        if (err) {
+            console.log("保存失败" + err);
+        }
+        else {
+            console.log("Res:" + response);
+            res.send('保存成功');
+        }
 
-    // });
+    });
 
 }
