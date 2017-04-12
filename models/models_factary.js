982c3bd932601a30e845a0216805fc36d586a01f
yangttlxl@sina.com <yangttlxl@sina.com>
2017-04-12 17:04:59 +0800

    提交

--- models/models_factary.js
+++ models/models_factary.js
@@ -19,7 +19,7 @@ var PageQuery = async (page, pageSize, Model, populate, queryParams, sortParams)
 
     $page.pageCount = (count - 1) / pageSize + 1;
     $page.results = records;
-
+d
     return $page;
 };
 
